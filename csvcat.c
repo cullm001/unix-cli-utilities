6038450aed99caabe0d6b4b49f1bddbe65b3c32e
Chris <cullm001@ucr.edu>
2024-08-24 20:02:45 -0700

    parses cli arguments

--- csvcat.c
+++ csvcat.c
@@ -1,127 +1,143 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
+
 #define MAXCHAR 1000
 
-
-int main(int argc, char *argv[]){
-    
-    if (argc == 1){
-        printf("%s","Error: File argument not passed in");
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("Error: File argument not passed in\n");
         return 1;
     }
-    
-    char row[MAXCHAR];
-    char header[1000];
 
-    FILE* stream = fopen(argv[1], "r");
-    if (stream == NULL){
+    int numCSV = 0;
+    char bytes[10] = "";
+    char outdir[30] = "./";
+    int threads = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if (strstr(argv[i], "--bytes=") != NULL) {
+            strncpy(bytes, argv[i] + 8, strlen(argv[i]) - 8);
+            bytes[strlen(argv[i]) - 8] = '\0';
+        }
+        else if (strstr(argv[i], "--threads") != NULL) {
+            threads = 1;
+        } 
+        else if (strstr(argv[i], "--outdir=") != NULL) {
+            strncpy(outdir, argv[i] + 9, strlen(argv[i]) - 9);
+            outdir[strlen(argv[i]) - 9] = '\0';
+        } 
+        else {
+            numCSV++;
+        }
+    }
+
+    printf("%d\n", numCSV);
+
+    char row[MAXCHAR];
+    char header[MAXCHAR];
+
+    FILE *stream = fopen(argv[1], "r");
+    if (stream == NULL) {
         printf("Error: file %s NOT found\n", argv[1]);
         return 1;
     }
     fgets(header, MAXCHAR, stream);
     fclose(stream);
 
-    int numCSV;
-    if (strstr(argv[argc-2],"--bytes=") == NULL){
-        numCSV = argc - 1;
-    }
-    else{
-        numCSV = argc - 3;
-    }
-
-    for (int i = 2; i < numCSV; i++){
-        FILE* stream = fopen(argv[i], "r");
-        if (stream == NULL){
-            printf("Error: file %s not found\n", argv[1]);
+    for (int i = 2; i <= numCSV; i++) {
+        stream = fopen(argv[i], "r");
+        if (stream == NULL) {
+            printf("Error: file %s not found\n", argv[i]);
             return 1;
-        }  
+        }
         fgets(row, MAXCHAR, stream);
-        if (strcmp(header,row) != 0){
-            printf("Error: %s contains different header schema\n", argv[i]);
+        if (strcmp(header, row) != 0) {
+            printf("Error: %s contains a different header schema\n", argv[i]);
             return 1;
         }
         fclose(stream);
-
     }
 
-    if (strstr(argv[argc-2],"--bytes=") == NULL){
+    if (strlen(bytes) == 0) {  
         printf("%s", header);
-        for (int i = 1; i < argc; i++){
-            FILE* stream = fopen(argv[i], "r");
-            fgets(row, MAXCHAR, stream);
-
-            while (true){
-                fgets(row, MAXCHAR, stream);
-                if (feof(stream)) break;
+        for (int i = 1; i <= numCSV; i++) {
+            stream = fopen(argv[i], "r");
+            fgets(row, MAXCHAR, stream); 
+            while (fgets(row, MAXCHAR, stream) != NULL) {
                 printf("%s", row);
             }
             fclose(stream);
         }
-    }
+    } else if (threads == 0) {
+        printf("Break up into files\n");
 
-    else{
-        char bytes[20];
-        strcpy(bytes, argv[argc - 2]);
+        long long fileCap = atoll(bytes);
 
-        char str_size[6];
-        strncpy(str_size, bytes+8, strlen(bytes)-1 - 8 );
-        long long fileCap = atoi(str_size);
-
-        if (bytes[strlen(bytes)-1] == 'G' ){
+        if (bytes[strlen(bytes) - 1] == 'G') {
             fileCap *= 1024 * 1024 * 1024;
-        }
-        else if(bytes[strlen(bytes)-1] == 'M') {
-            fileCap *= 1024*1024;
-        }
-        else if(bytes[strlen(bytes)-1] == 'K') {
+        } else if (bytes[strlen(bytes) - 1] == 'M') {
+            fileCap *= 1024 * 1024;
+        } else if (bytes[strlen(bytes) - 1] == 'K') {
             fileCap *= 1024;
-        }
-        else if (bytes[strlen(bytes)-1] != 'B'){
-            printf("%s","Error: incorrect file size format");
+        } else if (bytes[strlen(bytes) - 1] != 'B') {
+            printf("Error: incorrect file size format\n");
             return 1;
         }
+
+        printf("File Cap: %lld\n", fileCap);
 
         int currentSize = 0;
         int currentInputFile = 1;
         int currentOutputFile = 1;
-        char fileName[20]; 
+        char fileName[100];
 
-        FILE* ifstream = fopen(argv[1], "r");
-        sprintf(fileName, "output_%d.csv", currentOutputFile);
-        FILE* ofstream = fopen(fileName, "w");
+        FILE *ifstream = fopen(argv[currentInputFile], "r");
+        if (ifstream == NULL) {
+            printf("Error: file %s not found\n", argv[currentInputFile]);
+            return 1;
+        }
 
-        while (true){
-            fgets(row, MAXCHAR, ifstream);
+        sprintf(fileName, "%s/output_%d.csv", outdir, currentOutputFile);
+        FILE *ofstream = fopen(fileName, "w");
+
+        fprintf(ofstream, "%s", header); 
+
+        while (true) {
+            if (fgets(row, MAXCHAR, ifstream) == NULL) {
+                fclose(ifstream);
+                currentInputFile++;
+                if (currentInputFile > numCSV) {
+                    break;
+                }
+                ifstream = fopen(argv[currentInputFile], "r");
+                if (ifstream == NULL) {
+                    printf("Error: file %s not found\n", argv[currentInputFile]);
+                    break;
+                }
+                fgets(row, MAXCHAR, ifstream);  
+                continue;
+            }
+
             int lineSize = strlen(row);
 
-            if (feof(ifstream)){
-                if (currentInputFile == numCSV)
-                    break;
-                currentInputFile++;
-                fclose(ifstream);
-                FILE* ifstream = fopen(argv[currentInputFile], "r");
-                fgets(row, MAXCHAR, ifstream);
-
-            }
-
-            else if (lineSize + currentSize <= fileCap){
-                currentSize+=lineSize;
-            }
-
-            else if (lineSize + currentSize > fileCap){
+            if (lineSize + currentSize <= fileCap) {
+                fprintf(ofstream, "%s", row);
+                currentSize += lineSize;
+            } else {
                 fclose(ofstream);
                 currentOutputFile++;
-                sprintf(fileName, "output_%d.csv", currentOutputFile);
-                FILE* ofstream = fopen(fileName, "w");
+                sprintf(fileName, "%s/output_%d.csv", outdir, currentOutputFile);
+                ofstream = fopen(fileName, "w");
+                fprintf(ofstream, "%s", header); 
                 currentSize = lineSize;
                 fprintf(ofstream, "%s", row);
-
             }
+        }
 
-        }        
+        fclose(ofstream);
     }
-    
+
     return 0;
 }
